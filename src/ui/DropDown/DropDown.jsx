c65a9449427c1dc1962c6aa0d7b6965291ccda33
Niakrist <ak_88dron@inbox.ru>
2024-12-02 16:56:50 +0300

    feat: emplement dropdown

--- src/ui/DropDown/DropDown.jsx
+++ src/ui/DropDown/DropDown.jsx
@@ -1,32 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Icon from "../../components/Icon/Icon";
-
+import { clsx } from "../../utils/clsx";
 import styles from "./DropDown.module.css";
 
 const DropDown = () => {
+  const [isOpenDropDown, setIsOpenDropDown] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpenDropDown(!isOpenDropDown);
+  };
   return (
     <>
-      <button className={styles.dropDown}>
-        Каталог <Icon name="dropDownIcon" className={styles.dropDownIcon} />
+      <button onClick={handleToggle} className={styles.dropDown}>
+        Каталог{" "}
+        <Icon
+          name="dropDownIcon"
+          className={clsx(
+            styles.dropDownIcon,
+            isOpenDropDown && styles.dropDownIconActive
+          )}
+        />
       </button>
-      <ul className={styles.dropDownList}>
-        <li className={styles.dropDownItem}>
-          <Link className={styles.dropDownLink}>Смесители</Link>
-        </li>
-        <li className={styles.dropDownItem}>
-          <Link className={styles.dropDownLink}>Душевые системы</Link>
-        </li>
-        <li className={styles.dropDownItem}>
-          <Link className={styles.dropDownLink}>Душевые стойки</Link>
-        </li>
-        <li className={styles.dropDownItem}>
-          <Link className={styles.dropDownLink}>Изливы</Link>
-        </li>
-        <li className={styles.dropDownItem}>
-          <Link className={styles.dropDownLink}>Аксессуары</Link>
-        </li>
-      </ul>
+      {isOpenDropDown && (
+        <ul className={styles.dropDownList}>
+          <li className={styles.dropDownItem}>
+            <Link className={styles.dropDownLink}>Смесители</Link>
+          </li>
+          <li className={styles.dropDownItem}>
+            <Link className={styles.dropDownLink}>Душевые системы</Link>
+          </li>
+          <li className={styles.dropDownItem}>
+            <Link className={styles.dropDownLink}>Душевые стойки</Link>
+          </li>
+          <li className={styles.dropDownItem}>
+            <Link className={styles.dropDownLink}>Изливы</Link>
+          </li>
+          <li className={styles.dropDownItem}>
+            <Link className={styles.dropDownLink}>Аксессуары</Link>
+          </li>
+        </ul>
+      )}
     </>
   );
 };
